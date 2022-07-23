cbf47592a3e7a85206361d563e4252146141b93d
mpenciak <matej.penciak@gmail.com>
2022-07-23 12:32:19 -0400

    start of baer proof

---
+++ src/baerproof.lean
@@ -0,0 +1,21 @@
+
+
+/-
+
+Hom(-, Hom(M, X)) <-> Hom ((_ ⊗ M), X)
+Hom(-, X) preserves monos, and reflects monos
+(X is a cogenerator),
+
+Hom(M, X) is injective iff 
+preserves mono iff 
+_ ⊗ M preserves mono iff
+M is flat
+
+Baer criterion: Suffices to check
+Hom(-, Hom(M, X)) preserves monos of the form
+I → R
+So we only need to check such morphisms for flatness
+
+where X is an injective cogenerator
+-/
+
